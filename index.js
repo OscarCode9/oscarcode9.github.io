4b2885f433b884550fcd1b30cba0a1461433e318
Oscar martinez <oscar.99.tris@gmail.com>
2018-02-21 00:15:27 -0600

    CORS add

--- index.js
+++ index.js
@@ -11,7 +11,24 @@ var users = require('./routes/users');
 const hbs = require('express-handlebars');
 var app = express();
 
+app.use(function (req, res, next) {
 
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://encryptar.herokuapp.com/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 const mysql2 = require('mysql');
