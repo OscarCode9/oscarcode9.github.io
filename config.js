e5288110fcb034e6fde012c68acf943cab51be3d
Oscar <oscar.99.tris@gmail.com>
2018-05-02 15:29:41 -0500

    CH&ang5e port

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    port: process.env.PORT || 3001,
+    port: process.env.PORT || 3000,
     db: process.env.MONGODB_URI || 'mongodb://triste9:tristeGDA13##1@ds153652.mlab.com:53652/heroku_m1ph3hm6',
     SECRET_TOKEN: 'miclavedetokens'
   }
